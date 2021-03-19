8ce17b0048f66a4336b7fff14248cb5fefc9fb83
zbaxter1 <zbub123@gmail.com>
2021-03-19 09:59:05 -0400

    Starting Dev Branch

---
+++ src/views/Header.js
@@ -0,0 +1,16 @@
+import react from 'react'
+import Login from '../components/login.js'
+import fetchrequests from '../fetchRequests.js'
+import Home from './Home.js'
+import { Nav, Navbar, NavLink} from 'react-bootstrap'
+
+
+const navView(){
+    
+    <Navbar fixed="top" />
+<Navbar bg="dark" variant="dark"/>
+<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+<NavLink href="#home">Home</NavLink>
+<NavLink href="#settings">Settings</NavLink>
+<NavLink href="#Profile">Profile</NavLink>
+};
