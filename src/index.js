7a0202cb827affce4de93d2dbef32088818e7c5d
GuitarGuyB <isaacben822@gmail.com>
2021-03-25 11:19:12 -0400

    Fixed basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="kwitter-zaska-afk">
+    <BrowserRouter basename="/kwitter-zaska-afk">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
